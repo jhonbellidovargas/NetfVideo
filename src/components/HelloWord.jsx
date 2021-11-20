d4503210f0d6dd3b37aa3d8054f4e213e45d2802
Jhon Bellido <jhon.bellido.27@unsch.edu.pe>
2021-11-20 16:44:03 -0500

    ESLint

---
+++ src/components/HelloWord.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../assets/styles/App.scss'
+
+const HelloWord = () => {
+    return (
+        <h1>Hello Word</h1>
+    )
+}
+
+export default HelloWord;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import HelloWord from './components/HelloWord'
+
+ReactDOM.render(
+    <HelloWord/>,
+    document.getElementById('app')
+)
